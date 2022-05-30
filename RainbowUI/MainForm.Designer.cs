fbb5a9bc05c76cbf993efb08d6105789e08f37c4
Scope <98496609+Scope03@users.noreply.github.com>
2022-05-30 16:57:53 -0400

    Add project files.

---
+++ RainbowUI/MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace RainbowUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RainbowPanel = new System.Windows.Forms.Panel();
+            this.DragPanel = new System.Windows.Forms.Panel();
+            this.LogoLabel = new System.Windows.Forms.Label();
+            this.CloseLabel = new System.Windows.Forms.Label();
+            this.minimizelabel = new System.Windows.Forms.Label();
+            this.PanelRainbow = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.DragPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.panel1.Controls.Add(this.DragPanel);
+            this.panel1.Controls.Add(this.RainbowPanel);
+            this.panel1.Location = new System.Drawing.Point(-3, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // RainbowPanel
+            // 
+            this.RainbowPanel.BackColor = System.Drawing.Color.Red;
+            this.RainbowPanel.Location = new System.Drawing.Point(3, 15);
+            this.RainbowPanel.Name = "RainbowPanel";
+            this.RainbowPanel.Size = new System.Drawing.Size(806, 34);
+            this.RainbowPanel.TabIndex = 1;
+            // 
+            // DragPanel
+            // 
+            this.DragPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.DragPanel.Controls.Add(this.minimizelabel);
+            this.DragPanel.Controls.Add(this.CloseLabel);
+            this.DragPanel.Controls.Add(this.LogoLabel);
+            this.DragPanel.Location = new System.Drawing.Point(3, 3);
+            this.DragPanel.Name = "DragPanel";
+            this.DragPanel.Size = new System.Drawing.Size(806, 35);
+            this.DragPanel.TabIndex = 2;
+            this.DragPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
+            this.DragPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
+            // 
+            // LogoLabel
+            // 
+            this.LogoLabel.AutoSize = true;
+            this.LogoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.LogoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoLabel.ForeColor = System.Drawing.Color.Red;
+            this.LogoLabel.Location = new System.Drawing.Point(3, 9);
+            this.LogoLabel.Name = "LogoLabel";
+            this.LogoLabel.Size = new System.Drawing.Size(121, 25);
+            this.LogoLabel.TabIndex = 1;
+            this.LogoLabel.Text = "Rainbow UI";
+            this.LogoLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
+            this.LogoLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
+            // 
+            // CloseLabel
+            // 
+            this.CloseLabel.AutoSize = true;
+            this.CloseLabel.BackColor = System.Drawing.Color.Transparent;
+            this.CloseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseLabel.ForeColor = System.Drawing.Color.White;
+            this.CloseLabel.Location = new System.Drawing.Point(774, 9);
+            this.CloseLabel.Name = "CloseLabel";
+            this.CloseLabel.Size = new System.Drawing.Size(26, 25);
+            this.CloseLabel.TabIndex = 2;
+            this.CloseLabel.Text = "X";
+            this.CloseLabel.Click += new System.EventHandler(this.CloseLabel_Click);
+            // 
+            // minimizelabel
+            // 
+            this.minimizelabel.AutoSize = true;
+            this.minimizelabel.BackColor = System.Drawing.Color.Transparent;
+            this.minimizelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizelabel.ForeColor = System.Drawing.Color.White;
+            this.minimizelabel.Location = new System.Drawing.Point(749, 9);
+            this.minimizelabel.Name = "minimizelabel";
+            this.minimizelabel.Size = new System.Drawing.Size(19, 25);
+            this.minimizelabel.TabIndex = 3;
+            this.minimizelabel.Text = "-";
+            this.minimizelabel.Click += new System.EventHandler(this.minimizelabel_Click);
+            // 
+            // PanelRainbow
+            // 
+            this.PanelRainbow.Enabled = true;
+            this.PanelRainbow.Tick += new System.EventHandler(this.PanelRainbow_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rainbow UI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.DragPanel.ResumeLayout(false);
+            this.DragPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel DragPanel;
+        private System.Windows.Forms.Label minimizelabel;
+        private System.Windows.Forms.Label CloseLabel;
+        private System.Windows.Forms.Label LogoLabel;
+        private System.Windows.Forms.Panel RainbowPanel;
+        private System.Windows.Forms.Timer PanelRainbow;
+    }
+}
+
---
+++ RainbowUI/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RainbowUI
+{
+    public partial class MainForm : Form
+    {
+        Point lastPoint;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DragPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            //For dragging
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void DragPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            //For dragging
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void CloseLabel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimizelabel_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Idk put stuff in here if you want
+        }
+
+        //<<<<<< Rainbow timer >>>>>>>
+        int r = 255, g = 0, b = 0;
+        private void PanelRainbow_Tick(object sender, EventArgs e)
+        {
+            RainbowPanel.BackColor = Color.FromArgb(r, g, b);
+
+            if (r > 0 && b == 0) 
+            {
+                r--;
+                g++;
+            }
+            if (g > 0 && r == 0) 
+            {
+                g--;
+                b++;
+            }
+            if (b > 0 && g == 0)
+            {
+                b--;
+                r++;
+            }
+        }
+        //<<<<<< Rainbow timer >>>>>>>
+    }
+}
